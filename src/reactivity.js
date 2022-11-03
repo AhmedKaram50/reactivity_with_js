9b097d90e6b8542222e52c263efd4828d08e875b
Ahmed_Karam <ahmed.karam@lexmodo.com>
2022-11-03 17:35:01 +0200

    create nodes from JSON

--- src/reactivity.js
+++ src/reactivity.js
@@ -1,4 +1,4 @@
-const FIRST_ATTR_NAME = 'data'
+const FIRST_ATTR_NAME = "data";
 
 const data = {
   names: [
@@ -9,16 +9,71 @@ const data = {
     { name: "ahmed5" },
     { name: "ahmed6" },
   ],
-  ages: [
-    55,
-    60,
-    1,
-    80,
-    88,
-  ]
+  ages: [55, 60, 1, 80, 88],
 };
 
-function looping() {
+const iterate = (obj) => {
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+    console.log(`key: ${key}, value: ${obj[key]}`)
+        iterate(obj[key])
+      }
+  })
+}
+
+function createNodesJSON(nodeName, attributes, ...args) {
+  let children = args.length ? [].concat(...args) : null;
+  return { nodeName, attributes, children };
+}
+
+function createElementFromNodesObject(nodesObject) {
+  const { nodeName, attributes, children } = nodesObject;
+  console.log(nodesObject); //! console here
+  const mainElement = document.createElement(nodeName);
+  for (let attr in attributes) {
+    mainElement.setAttribute(attr, attributes[attr]);
+  }
+
+  iterate(children[1])
+
+  // let current = children?.children;
+  // while (current.hasOwnProperty("children")) { // * if the element still has more children
+  //   // console.log(current, current.hasOwnProperty("children"));
+  //   current = child.children;
+  // }
+
+
+  // children.forEach((child) => {
+  //   console.log(child)
+  //   if (typeof child === "object") {
+      
+     
+  //   }
+
+    
+  // });
+  return mainElement;
+}
+
+console.log(
+  createElementFromNodesObject(
+    createNodesJSON("div", { id: "test", class: "navbar" }, [
+      "text in the div",
+      createNodesJSON(
+        "h1",
+        { class: "main-heading" },
+        createNodesJSON("span", {class: "span-class" }, "span text")
+      ),
+      createNodesJSON(
+        "h1",
+        { class: "main-heading" },
+        createNodesJSON("span", {class: "span-class" }, "span text")
+      ),
+    ])
+  )
+);
+
+function loopingOnDataForElements() {
   // ! change name here
   const dataForElements = document.querySelectorAll(`[${FIRST_ATTR_NAME}-for]`);
   dataForElements.forEach((el) => {
@@ -28,11 +83,14 @@ function looping() {
     if (attributeValueAsArray.length !== 3 || operator !== "in")
       return console.warn("Syntax Error From Reactivity System");
     if (!data.hasOwnProperty(items)) return console.error(`${items} not found`);
-    el.querySelectorAll(`[${FIRST_ATTR_NAME}-bind]`)
-    console.log(el.querySelectorAll(`[${FIRST_ATTR_NAME}-bind]`));
+    const bindElements = el.querySelectorAll(`[${FIRST_ATTR_NAME}-bind]`);
+    const elementsInsideFor = el.children;
+    data[items].forEach((i) => {
+      el.insertAdjacentHTML("beforeend", elementsInsideFor[1].outerHTML);
+    });
   });
 }
-looping();
+loopingOnDataForElements();
 
 document.getElementById("add").addEventListener("click", () => {
   console.log("ahh");
